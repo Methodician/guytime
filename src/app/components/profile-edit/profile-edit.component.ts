52671c7f7647fdc9c317a3cc1dc21e03cc130c0b
Methodician <methodician@gmail.com>
2020-06-16 13:49:02 -0700

    made bio connected to a thing

--- src/app/components/profile-edit/profile-edit.component.ts
+++ src/app/components/profile-edit/profile-edit.component.ts
@@ -15,6 +15,10 @@ export class ProfileEditComponent implements OnInit {
   avatarUrl: string | ArrayBuffer =
     'https://material.angular.io/assets/img/examples/shiba1.jpg';
 
+  bio = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sollicitudin tempor id eu nisl nunc mi ipsum faucibus. Sapien et ligula ullamcorper malesuada proin. Habitant morbi tristique senectus et netus et malesuada. Tortor consequat id porta nibh venenatis cras. Tempus iaculis urna id volutpat lacus. Suspendisse ultrices gravida dictum fusce. Ipsum dolor sit amet consectetur adipiscing elit. Lectus vestibulum mattis ullamcorper velit sed ullamcorper. Mauris pharetra et ultrices neque ornare aenean euismod elementum. Adipiscing commodo elit at imperdiet dui accumsan sit amet.
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sollicitudin tempor id eu nisl nunc mi ipsum faucibus. Sapien et ligula ullamcorper malesuada proin. Habitant morbi tristique senectus et netus et malesuada. Tortor consequat id porta nibh venenatis cras. Tempus iaculis urna id volutpat lacus. Suspendisse ultrices gravida dictum fusce. Ipsum dolor sit amet consectetur adipiscing elit. Lectus vestibulum mattis ullamcorper velit sed ullamcorper. Mauris pharetra et ultrices neque ornare aenean euismod elementum. Adipiscing commodo elit at imperdiet dui accumsan sit amet.`;
+
   headerOptions: IHeaderOption[];
 
   selectedInterests = ['outdoors'];
