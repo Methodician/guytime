6185e3049fc589959617dc23f8e57fc1a05c26f4
Methodician <methodician@gmail.com>
2020-07-17 10:28:54 -0700

    Added functionality to me-detail and cleaned up profile-detail

--- src/app/components/profile/me-detail/me-detail.component.ts
+++ src/app/components/profile/me-detail/me-detail.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { IHeaderOption } from '@app/components/header/header.component';
+import { BehaviorSubject } from 'rxjs';
+import { UserI } from '@app/models/user';
+import { UserService } from '@app/services/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'gtm-me-detail',
   templateUrl: './me-detail.component.html',
-  styleUrls: ['./me-detail.component.scss']
+  styleUrls: ['./me-detail.component.scss'],
 })
 export class MeDetailComponent implements OnInit {
+  headerOptions: IHeaderOption[];
+  user$ = new BehaviorSubject<UserI>(null);
+  avatarUrl$ = new BehaviorSubject<string>('assets/icons/square_icon.svg');
 
-  constructor() { }
+  promptEvent;
+
+  constructor(private userSvc: UserService, private router: Router) {}
 
   ngOnInit(): void {
+    this.user$ = this.userSvc.loggedInUser$;
+    this.avatarUrl$ = this.userSvc.getLoggedInAvatarUrl();
+    this.headerOptions = [
+      {
+        iconName: 'edit',
+        optionText: 'Edit Details',
+        isDisabled: false,
+        onClick: this.onEditClicked,
+      },
+      {
+        iconName: 'people',
+        optionText: 'Connections',
+        isDisabled: true,
+        onClick: this.logClicked,
+      },
+    ];
   }
 
+  onEditClicked = () => this.router.navigate(['/me/edit']);
+  logClicked = () => console.log('clicked');
 }
