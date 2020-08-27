c7126623e5de52d19ef882721453a0d09206b5d0
Chisato Murakami <kiwipom1031@gmail.com>
2020-08-27 14:34:35 -0700

    add chatUsers$ to set two users, add updateHeader() to change header text

--- src/app/components/chat/chat-detail/chat-detail.component.ts
+++ src/app/components/chat/chat-detail/chat-detail.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { HeaderService } from '@app/services/header.service';
 import { ActivatedRoute } from '@angular/router';
-import { Subject } from 'rxjs';
+import { Subject, BehaviorSubject, combineLatest } from 'rxjs';
+import { takeUntil, map } from 'rxjs/operators';
 import { ChatService } from '@services/chat.service';
+import { UserService } from '@app/services/user.service';
+import { UserI } from '@models/user';
 
 @Component({
   selector: 'gtm-chat-detail',
@@ -11,11 +14,13 @@ import { ChatService } from '@services/chat.service';
 })
 export class ChatDetailComponent implements OnInit {
   private unsubscribe$: Subject<void> = new Subject();
+  chatUsers$ = new BehaviorSubject<UserI[]>([]);
 
   constructor(
     private headerSvc: HeaderService,
     private route: ActivatedRoute,
     private chatSvc: ChatService,
+    private userSvc: UserService,
   ) {}
 
   ngOnDestroy(): void {
@@ -29,22 +34,62 @@ export class ChatDetailComponent implements OnInit {
       if (params['id']) {
         const chatId = params['id'];
         // const docRef = this.afs.collection('chats').doc<ChatGroupI>(chatId); // please don't delete for now
-        const docRef = this.chatSvc.chatGroupRef(chatId);
-        const chatObservable$ = docRef.valueChanges();
-        chatObservable$.subscribe(chat => console.log(chat.participantIds));
+        // const docRef = this.chatSvc.chatGroupRef(chatId);
+        // const chatObservable$ = docRef.valueChanges();
+        const chatObservable$ = this.getChatObservables(chatId);
+        // chatObservable$.subscribe(chat => console.log(chat.participantIds));
+        chatObservable$.subscribe(chatGroup =>
+          this.watchChatUsers(chatGroup.participantIds),
+        );
+        setTimeout(() => this.updateHeader());
       }
     });
   }
 
-  getChatObservables = () => {
+  getChatObservables = (chatId: string) => {
     // return the results of piping chatSvc.chatGroupRef through valueChanges and map takeUntil (like in OtherConnectionsListComponent)
+    return this.chatSvc
+      .chatGroupRef(chatId)
+      .valueChanges()
+      .pipe(
+        map(chatGroup => ({ ...chatGroup, chatId })), //??
+        takeUntil(this.unsubscribe$),
+      );
   };
 
   watchChatUsers = (userIds: string[]) => {
-    // create an observable that emits an array of UserI objects, as in watchUserContacts inside OtherConnectionsListComponent
     // Feel free to start by just writing code inside ngOnInit and logging results to console one step at a time
-    // As you have so far, awesome, if it's logging the array of ids to the console.
+    console.log(userIds);
+    // create an observable that emits an array of UserI objects, as in watchUserContacts inside OtherConnectionsListComponent
+    const userObservables = userIds.map(uid => this.getUserObservable(uid));
     // Might use those ids to call this method next but start by just logging the user DocRefs to console
+    console.log(userObservables);
     // Then log the observables they provide, then subscribe to each and log the users, then try to make the RxJS piping work to convert it all to an observable that emits an array of users
+
+    // delete this later, just for demonstration...?????
+    for (let user$ of userObservables) {
+      user$.subscribe(console.log);
+    }
+
+    const users$ = combineLatest(userObservables);
+    console.log(users$);
+    users$.subscribe(users => {
+      console.log(users);
+      this.chatUsers$.next(users);
+    });
+  };
+
+  getUserObservable = (uid: string) =>
+    this.userSvc
+      .userRef(uid)
+      .valueChanges()
+      .pipe(map(user => ({ ...user, uid }), takeUntil(this.unsubscribe$)));
+
+  updateHeader = () => {
+    this.chatUsers$.subscribe(users => {
+      const firstNames = users.map(user => user.fName);
+      const namesList = firstNames.join(', ');
+      this.headerSvc.setHeaderText(namesList);
+    });
   };
 }
