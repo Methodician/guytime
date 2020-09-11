0f3a6a00c765f1a5742b99b5dbbf9c6786492b01
Jacob Johnston <Methodician@tricias-mbp.lan>
2020-09-11 15:08:09 -0700

    Updated message model

--- src/app/models/message.ts
+++ src/app/models/message.ts
@@ -1,9 +1,9 @@
-import { Timestamp } from '@google-cloud/firestore';
+import { firestore } from 'firebase';
 
 export interface MessageI {
   id?: string;
   chatGroupId: string;
   senderId: string;
   content: string;
-  createdAt: Timestamp;
+  createdAt: firestore.FieldValue | firestore.Timestamp;
 }
