5e3878f8f9d859f017c1c8b44c873c4bf6cbfda9
Chisato Murakami <kiwipom1031@gmail.com>
2020-09-14 16:37:01 -0700

    add avatarUrl$ in chat-message to display users profile image

--- src/app/components/chat/chat-message/chat-message.component.ts
+++ src/app/components/chat/chat-message/chat-message.component.ts
@@ -15,6 +15,7 @@ export class ChatMessageComponent implements OnInit {
   @Input() chatMessage: MessageI;
   // @Input() chatMessage: MessageI; // later we'll switch it back to this
   user$ = new BehaviorSubject<UserI>(null);
+  avatarUrl$ = new BehaviorSubject<string>('assets/icons/square_icon.svg');
   constructor(private userSvc: UserService) {}
 
   ngOnDestroy(): void {
@@ -28,7 +29,12 @@ export class ChatMessageComponent implements OnInit {
       this.user$.next(user);
       // However, in the HTML template you can use Angulars async pipe like so:
       // <div>{{ (user$ | async)?.fName }}</div>
-      console.log(this.user$.value.fName);
+      //console.log(this.user$.value.fName);
+      const uid = this.user$.value.uid;
+      this.userSvc
+        .getAvatarUrl(uid)
+        .subscribe(url => this.avatarUrl$.next(url));
+      console.log(this.avatarUrl$);
     });
   }
 
