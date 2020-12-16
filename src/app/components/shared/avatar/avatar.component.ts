70e399b5d91ae5bf8b55bfc8134e66386dd52b9e
Jacob Johnston <Methodician@tricias-mbp.lan>
2020-12-16 11:58:46 -0800

    Made avatar component capable of taking raw storage url

--- src/app/components/shared/avatar/avatar.component.ts
+++ src/app/components/shared/avatar/avatar.component.ts
@@ -1,4 +1,10 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 import { ProfileImageSizeT } from '@app/models/user';
 import { UserService } from '@app/services/user.service';
 import { BehaviorSubject } from 'rxjs';
@@ -8,16 +14,24 @@ import { BehaviorSubject } from 'rxjs';
   templateUrl: './avatar.component.html',
   styleUrls: ['./avatar.component.scss'],
 })
-export class AvatarComponent implements OnInit {
+export class AvatarComponent implements OnChanges {
+  /** Used along with imageSize to get a URL for a user's avatar */
   @Input() fileName: string;
+  /** Used along with fileName to get a URL for a user's avatar */
   @Input() imageSize: ProfileImageSizeT;
+
+  /** When supplied, this will be used directly instead of getting the URL from APIs */
+  @Input() avatarFileUrl: string;
 
   avatarUrl$ = new BehaviorSubject('assets/icons/square_icon.svg');
 
   constructor(private userSvc: UserService) {}
 
-  ngOnInit(): void {
-    if (this.fileName) {
+  ngOnChanges(changes: SimpleChanges) {
+    console.log(changes);
+    if (!!changes.avatarFileUrl && !!changes.avatarFileUrl.currentValue) {
+      this.avatarUrl$.next(this.avatarFileUrl);
+    } else if (!!changes.fileName && !!changes.fileName.currentValue) {
       this.setUrl();
     }
   }
